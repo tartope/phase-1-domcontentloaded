ce9942b0034f1b15de51e1fc91854a6da150f567
Tunisia Artope <artope@yahoo.com>
2022-02-09 09:08:51 -0500

    commit tree e0e37c94440404ee95b44e8bed86a76e1ace8b17

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+//1. make event listener for DOMContentLoaded and add the 2 arguments.
+//2. make a variable to grab the element.
+//3. set the text content for the new variable.
+document.addEventListener('DOMContentLoaded', function(){
+    const p = document.getElementById('text');
+    p.textContent = 'This is really cool!';
+});
